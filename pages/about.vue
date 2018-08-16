878d7b53260c923f6b66720750e5ab416766bed3
Max Frederiksen <max.frederiksencoles@gmail.com>
2018-08-16 19:28:07 +0200

    commiting all files

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About us page</h1>
+    <p>This is the about us paragraph, with more text than usual.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
+
